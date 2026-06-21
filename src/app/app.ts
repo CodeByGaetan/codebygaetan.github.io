5a175452f6c7e4d1c230323c2211f1c67b6ef54c
Gaétan La Selve <codebygaetan@gmail.com>
2026-06-21 16:46:04 +0200

    add services and about sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Nav } from './layout/nav/nav';
 import { Hero } from './sections/hero/hero';
 import { Stats } from './sections/stats/stats';
+import { Services } from './sections/services/services';
+import { About } from './sections/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Hero, Stats],
+  imports: [Nav, Hero, Stats, Services, About],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
